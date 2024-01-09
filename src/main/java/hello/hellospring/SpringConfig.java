aa856454eafab57d4d04be17e05784de890ff09e
brandmin <overness@naver.com>
2024-01-09 14:43:18 +0900

    240109 Upload

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,13 +1,24 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
 // @Bean 메서드 제공, Spring 컨테이너에 의해 관리되는 구성클래스
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -17,6 +28,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository(); // 메모리멤버리파지토리를 위의 서비스를 엮어야함.
+       // return new MemoryMemberRepository(); // 메모리멤버리파지토리를 위의 서비스를 엮어야함.
+        return new JdbcMemberRepository(dataSource);
     }
 }
